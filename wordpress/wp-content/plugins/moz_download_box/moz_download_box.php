c09ec670dc142c6259bcf1e6e6e01bef7f51db5a
Tomer Cohen <tomerc+github.com@gmail.com>
2010-09-27 10:18:52 +0200

    Adding administration interface for feeds.

--- wordpress/wp-content/plugins/moz_download_box/moz_download_box.php
+++ wordpress/wp-content/plugins/moz_download_box/moz_download_box.php
@@ -12,35 +12,108 @@ License: A "Slug" license name e.g. GPL2
 
 /// Activation functions 
 register_activation_hook(__FILE__, 'moz_download_box_activation');
-add_action('moz_download_box_hourly_event', 'moz_download_box_prepare_tables');
-
+//add_action('moz_download_box_hourly_event', 'moz_download_box_prepare_tables');
+add_action('moz_download_box_fetch_feeds', 'moz_download_box_update_table');
 
 /// Shortcode hooks
 add_shortcode('moz-download-box', 'moz_download_box_button_shortcode');
 
 
 function moz_download_box_activation() {
-	add_option('moz_download_box_last_fetch', '0');
+	//add_option('moz_download_box_last_fetch', '0');
 
+	$options = array (
+		'thunderbird' => array(
+			'tags_url' => 'http://www.mozilla.com/includes/product-details/json/thunderbird_versions.json',
+			'builds_url' => 'http://www.mozilla.com/includes/product-details/json/thunderbird_primary_builds.json',
+			'timestamp' => 0),
+		'firefox' => array(
+//			'enabled' => false, 
+			'tags_url' => 'http://www.mozilla.com/includes/product-details/json/firefox_versions.json',
+			'builds_url' => 'http://www.mozilla.com/includes/product-details/json/firefox_primary_builds.json',
+			'timestamp' => 0));
+			
+	
+	
+//add_option('moz_download_box_feeds', serialize($options));  // Default configuration
+	add_option('moz_download_box_feeds', $options);  // Default configuration
+
+	
 	moz_download_box_construct_table();
 //	moz_download_box_update_table();
 
-	wp_schedule_event(time(), 'hourly', 'moz_download_box_hourly_event');
+//	wp_schedule_event(time(), 'hourly', 'moz_download_box_hourly_event');
+//	wp_schedule_event(time(), 'hourly', 'moz_download_box_fetch_feeds');
+
+//	moz_download_box_reschedule_fetch();
 }
 
-function moz_download_box_prepare_tables() {
+/*function moz_download_box_prepare_tables() {
 	// do something every hour
 	update_option('moz_download_box_last_fetch', time());
 //	moz_download_box_update_table();
 	
+}*/
+
+function moz_download_box_show_schedules() {
+//	$list = unserialize(get_option('moz_download_box_feeds'));
+	$list = get_option('moz_download_box_feeds');
+
+		
+	if (count($list) > 0) {
+
+		echo ("<table>");
+		echo ("  <tr>\n    <th>Feed</th>\n    <th>Last</th>\n    <th>Next</th>\n  </tr>");
+		foreach ($list as $feed=>$item) {
+			echo ("  <tr>\n    <td>$feed</td>\n    <td>". date_i18n(get_option('date_format') . ' ' . get_option('time_format'), $item['timestamp']) ."</td>\n");
+			echo ("    <td>". date_i18n(get_option('date_format') . ' ' . get_option('time_format'), wp_next_scheduled('moz_download_box_fetch_feeds', array($feed))) ."</td>\n");
+			echo ("  </tr>");
+		}
+		echo ("</table>");
+	}
+	else {
+		echo ("<p>No data been found.</p>");
+	}
 }
+
+function moz_download_box_reschedule_fetch() {
+	//wp_clear_scheduled_hook('moz_download_box_hourly_event');
+//	$options = unserialize(get_option('moz_download_box_feeds'));
+	$options = get_option('moz_download_box_feeds');
+	wp_clear_scheduled_hook('moz_download_box_fetch_feeds');
+	
+	foreach ($options as $name=>$feed) {
+		if (isset($feed['enabled']) && $feed['enabled'] == true) {
+			if (isset($feed['recurrance'])) {
+				switch ($feed['recurrance']) {
+					case 'daily':
+						moz_download_box_schedule($name, 'daily', time() + rand(0, 60 * 60 * 24)); break;
+					case 'twicedaily':
+						moz_download_box_schedule($name, 'twicedaily', time() + rand(0, 60 * 60 * 12)); break;
+					case 'hourly':
+					default: 
+						moz_download_box_schedule($name, 'hourly', time() + rand(0, 60 * 60)); break;
+				}
+			}
+			else	moz_download_box_schedule($name, 'hourly', time() + rand(0, 60 * 60)); break;
+		}
+	}
+}
+
+function moz_download_box_schedule($feed, $recurrance = 'daily', $timestamp) {
+	//wp_schedule_event($timestamp, $recurrance, 'moz_download_box_fetch_json_feeds', array($feed));
+	wp_schedule_event($timestamp, $recurrance, 'moz_download_box_fetch_feeds', array($feed));
+}
+
 
 /// Deactivation functions 
 register_deactivation_hook(__FILE__, 'moz_download_box_deactivation');
 
 function moz_download_box_deactivation() {
-	wp_clear_scheduled_hook('moz_download_box_hourly_event');
-	delete_option('moz_download_box_last_fetch');
+	//wp_clear_scheduled_hook('moz_download_box_hourly_event');
+	wp_clear_scheduled_hook('moz_download_box_fetch_feeds');
+	delete_option('moz_download_box_last_fetch'); // TBD: Remove me
+	delete_option('moz_download_box_feeds');
 	moz_download_box_destruct_table();
 }
 
@@ -51,25 +124,170 @@ function moz_download_box_plugin_menu() {
   add_options_page('Moz Download Box Plugin Options', 'MozDownloadBox', 'manage_options', 'Moz_Download_Box', 'moz_download_box_plugin_options');
 }
 
+function moz_download_box_show_feeds_form() {
+	if (isset($_POST['manage_feed']) && isset($_POST['feed'])) moz_download_box_admin_manage_feeds();
+	else {
+	
+//		$list = unserialize(get_option('moz_download_box_feeds'));
+		$list = get_option('moz_download_box_feeds');
+
+		//echo ("list = ". get_option('moz_download_box_feeds') ." dump "); var_dump($list);
+	
+		//echo ('<form method="post" action="http://localhost/~tomer/1/form.php" name="manage_feeds" id="manage_feeds">');
+		echo ('<form method="post" action="" name="manage_feeds" id="manage_feeds">');
+		if (count($list) > 0) foreach ($list as $feed=>$item) {
+			echo ("<fieldset><legend>Feed: $feed</legend>");
+			echo ("<label for='feed[$feed][tags]'>Tags URL:</label    ><input name='feed[$feed][tags]'   id='feed[$feed][tags]' type='text' value='{$item['tags_url']}' />");
+			echo ("<label for='feed[$feed][builds]'>Builds URL:</label    ><input name='feed[$feed][builds]'   id='feed[$feed][builds]' type='text' value='{$item['builds_url']}' />");
+	//		echo ("<label for='feed[$feed][builds]'>Builds URL:</label><input   name=\"feed[$feed][builds]\" id=\"feed[$feed .'][builds]\" type="'text' value="'. $item['builds_url'] .'" />');
+
+			//echo ("<input name='feed[$feed][delete]' type='button' onclick='confirm(\"Are you sure?\");' value='Delete' />");
+			
+			echo ("<p><input type='checkbox' name='feed[$feed][enabled]' id='feed[$feed][enabled]' "); 
+			if (wp_next_scheduled('moz_download_box_fetch_feeds', array($feed))) echo ('checked=\'checked\'');
+			echo (' />');
+
+			echo ("<label for='feed[$feed][enabled]'>Enabled?</label> (Next fetch: ". wp_next_scheduled('moz_download_box_fetch_feeds', array($feed)) .") </p>");
+
+			echo ("<p><input type='checkbox' name='feed[$feed][delete]' id='feed[$feed][delete]' /><label for='feed[$feed][delete]'>Delete?</label> Warning: There is no undo.</p>");
+
+
+			echo ('</fieldset>');
+		}
+		echo ('<fieldset><legend>Add new feed</legend>');
+		echo ('<label for="feed[new][name]">Feed name:</label   ><input name="feed[new][name]"   id="feed[new][name]"   type="text" value="" />');
+		echo ('<label for="feed[new][tags]">Tags URL:</label    ><input name="feed[new][tags]"   id="feed[new][tags]"   type="text" value="" />');
+		echo ('<label for="feed[new][builds]">Builds URL:</label><input name="feed[new][builds]" id="feed[new][builds]" type="text" value="" />');
+		echo ('</fieldset>');
+		
+		echo ('<input type="submit" name="manage_feed" /></from>');
+	}
+}
+
+function moz_download_box_admin_manage_feeds() {
+//	$list = unserialize(get_option('moz_download_box_feeds'));
+	$list = get_option('moz_download_box_feeds');
+
+	//var_dump ($_POST);
+	
+	if (isset($_POST['manage_feed']) && isset($_POST['feed'])) {
+		foreach ($_POST['feed'] as $tag=>$item) {
+			if ($tag == 'new') {
+				if (isset($item['tags']) && $item['tags'] != '' && $item['builds'] != '' && $item['name'] != '') {
+					echo ("<p>Creating new feed {$item['name']}...</p>");
+					$list[$item['name']]['tags_url'] = $item['tags'];
+					$list[$item['name']]['builds_url'] = $item['builds'];
+					$list[$item['name']]['timestamp'] = 0;
+				}
+			}
+			else {
+				if (isset($item['delete'])) {
+					echo ("<p>Unscheduling $tag...</p>");
+					wp_unschedule_event(wp_next_scheduled('moz_download_box_fetch_feeds', array($tag)), 'moz_download_box_fetch_feeds', array($tag));
+					echo ("<p>Deleting <em>$tag</em>... :(</p>");
+					unset ($list[$tag]);
+				}
+				else {
+					if ($list[$tag]['tags_url'] != $item['tags']) {
+						echo ("<p>Updating <em>$tag</em> tags url...</p>");
+						$list[$tag]['tags_url'] = $item['tags'];
+					}
+					if ($list[$tag]['builds_url'] != $item['builds']) {
+						echo ("<p>Updating <em>$tag</em> builds url...</p>");
+						$list[$tag]['builds_url'] = $item['builds'];
+					}
+					
+					// If is scheduled and not enabled
+					if (!isset($item['enabled']) && wp_next_scheduled('moz_download_box_fetch_feeds', array($tag)) > 0) {
+//						echo ("<p>(". wp_next_scheduled('moz_download_box_fetch_feeds', array($tag)) .")</p>"); var_dump (wp_next_scheduled('moz_download_box_fetch_feeds', array($tag)));
+						echo ("<p>Unscheduling $tag...</p>");
+						//wp_clear_scheduled_hook('moz_download_box_hourly_event', array($tag));
+						wp_unschedule_event(wp_next_scheduled('moz_download_box_fetch_feeds', array($tag)), 'moz_download_box_fetch_feeds', array($tag));
+//						echo ("<p>(". wp_next_scheduled('moz_download_box_fetch_feeds', array($tag)) .")</p>"); var_dump (wp_next_scheduled('moz_download_box_fetch_feeds', array($tag)));
+					}
+					else if (isset($item['enabled']) && !wp_next_scheduled('moz_download_box_fetch_feeds', array($tag)) > 0) {
+						moz_download_box_schedule($tag, 'hourly', time() + rand(0, 60 * 60));
+						echo ("<p>Scheduling $tag (". wp_next_scheduled('moz_download_box_fetch_feeds', array($tag)) .")...</p>");
+					}
+				}
+			}
+			
+		}
+//	var_dump ($list);
+//	update_option('moz_download_box_feeds', serialize($list));
+	update_option('moz_download_box_feeds', $list);
+
+	}
+	else echo ("Nothing to do.");
+}
+
 function moz_download_box_plugin_options() {
 
   if (!current_user_can('manage_options'))  {
     wp_die( __('You do not have sufficient permissions to access this page.') );
   }
   
-    moz_download_box_update_table('http://www.mozilla.com/includes/product-details/json/firefox_primary_builds.json', 'http://www.mozilla.com/includes/product-details/json/firefox_versions.json');
+  if (isset($_REQUEST['action'])) $action = $_REQUEST['action'];
+  else $action=NULL;
+  
+  switch ($action) {
+	  case 'reschedule': 
+		echo ("<p>Rescheduling all feeds...</p>");
+		moz_download_box_reschedule_fetch();   
+		moz_download_box_show_schedules();
+		break;
+	  case 'forcefetch':
+		echo ("<p>Fetching all feeds...</p>");
+		moz_download_box_force_fetch_all();
+		break;
+	  case 'destroy':
+	    echo ("<p>Destroying all feeds...</p>");
+	    moz_download_box_deactivation();
+	    moz_download_box_activation();
+	    break;
+	  default: 
+		moz_download_box_admin_display();
+		break;
+  }
+}
+
+function moz_download_box_admin_display() {
+    //moz_download_box_update_table('http://www.mozilla.com/includes/product-details/json/firefox_primary_builds.json', 'http://www.mozilla.com/includes/product-details/json/firefox_versions.json');
+    
+    //moz_download_box_update_table('firefox');
     
     
 //  $timezone_format = _x('Y-m-d G:i:s', 'timezone date format');
 
-  echo '<div class="wrap">';
-  echo '<p dir="ltr">Last fetch of product-details: '. date_i18n(get_option( 'time_format') /*__('F j, Y g:i a')*/, get_option('moz_download_box_last_fetch'), true) .'</p>';
+  echo '<div class="wrap" dir="ltr">';
   
-echo (moz_download_box_draw_buttons('LATEST_FIREFOX_VERSION', 'he'));
+  echo ('<fieldset><legend>Schedules</legend>');
+  moz_download_box_show_schedules();
+  echo ('<form name="reschedule" method="post"><button name="action" value="reschedule">Reschedule All feeds</button></form></fieldset>');
+  echo ('<form name="force-fetch" method="post"><button name="action" value="forcefetch">Force fetch</button></form></fieldset>');
+  echo ('<form name="destroy-feeds" method="post"><button name="action" value="destroy">Destoy data</button></form></fieldset>');
+ 
+//  echo '<p dir="ltr">Last fetch of product-details: '. date_i18n(get_option( 'time_format') /*__('F j, Y g:i a')*/, get_option('moz_download_box_last_fetch'), true) .'</p>';
+
+  moz_download_box_show_feeds_form();
+
+  if ((isset($_REQUEST['action'])) && ($_REQUEST['action'] == 'debug')) {
+	  echo ("$print => <pre>");
+	  $print = get_option('moz_download_box_feeds');
+	  //var_dump(unserialize($print));
+	  echo ("</pre>");
+  }
   
-  echo ('<link rel="stylesheet" type="text/css" href="http://127.0.0.1/~tomer/moz-wp3/wp-content/plugins/moz_download_box/style.css" />');
+/*  var_dump(moz_download_box_get_feed_details('firefox'));
+  echo ("</pre>");*
   
-  echo '</div>';
+
+/*  echo (moz_download_box_draw_buttons('LATEST_FIREFOX_VERSION', 'he'));
+  echo ('<link rel="stylesheet" type="text/css" href="http://127.0.0.1/~tomer/moz-wp3/wp-content/plugins/moz_download_box/style.css" />');*/
+  
+  moz_download_box_show_tags();
+  
+  echo ("</div>");
   
 
 }
@@ -140,16 +358,22 @@ function moz_download_box_construct_table() {
 function moz_download_box_destruct_table() {
   global $wpdb;
   
-  $wpdb->query("DROP TABLE {$wpdb->prefix}moz_download_box};");
+  $wpdb->query("DROP TABLE {$wpdb->prefix}moz_download_box;");
 
 }
 
 
 function moz_download_box_fetch_tag_information($url = 'http://www.mozilla.com/includes/product-details/json/firefox_versions.json') {
+
+//	echo ("moz_download_box_fetch_tag_information($url);");//REMOVE ME
+	
   return json_decode(file_get_contents($url), true);
 }
 
 function moz_download_box_fetch_version_information($url = 'http://www.mozilla.com/includes/product-details/json/firefox_primary_builds.json') {
+
+//	echo ("moz_download_box_fetch_version_information($url);"); //REMOVE ME
+
   return json_decode (file_get_contents($url), true);
 }
 
@@ -196,25 +420,102 @@ function moz_download_box_update_table() {
   }
 }*/
 
-function moz_download_box_update_table($primary_builds_url, $versions_url) {
+function moz_download_box_get_feed_details($feed) {
+//	$out = unserialize(get_option('moz_download_box_feeds'));
+	$out = get_option('moz_download_box_feeds');
+	
+	if (isset($out[$feed])) return $out[$feed];
+	else return false;
+}
+function moz_download_box_update_feed_timestamp($feed, $timestamp) {
+	$out = get_option('moz_download_box_feeds');
+//	$out = unserialize(get_option('moz_download_box_feeds'));
+
+//	echo ("==="); var_dump(get_option('moz_download_box_feeds')); // debug 
+
+	if (isset($out[$feed])) { 
+		$out[$feed]['timestamp'] = $timestamp;
+		update_option('moz_download_box_feeds', $out);
+//		update_option('moz_download_box_feeds', serialize($out));
+	}
+	else return false; 
+}
+
+function moz_download_box_show_tags() {
+//	$list = unserialize(get_option('moz_download_box_feeds'));
+	$list = get_option('moz_download_box_feeds');
+	
+	$tags = array();
+	
+	foreach ($list as $item) {
+		if (isset($item['tags']) && count($item['tags']) > 0) $tags = array_merge($item['tags'], $tags);
+	}
+	
+	if (count($tags) > 0) {
+		echo ("<table><tr><th>Feed</th><th>Tag</th><th>Version</th></tr>");
+		foreach ($list as $feed=>$item) {
+			if (isset($item['tags']) && count($item['tags']) > 0)
+				foreach ($item['tags'] as $tag=>$version) {
+					echo ("<tr><td>$feed</td><td>$tag</td><td>$version</td></tr>");
+			}
+		}
+		echo ("</table>");
+	}
+	else 
+		echo ("No tags has been fetched... yet.");
+}
+
+function moz_download_box_update_feed_tags($feed, $tags) {
+//	$options = unserialize(get_option('moz_download_box_feeds'));
+	$options = get_option('moz_download_box_feeds');
+	
+	$options[$feed]['tags'] = $tags; 
+	
+//	update_option('moz_download_box_feeds', serialize($options));
+	update_option('moz_download_box_feeds', $options);
+
+}
+
+function moz_download_box_force_fetch_all() {
+//	$list = unserialize(get_option('moz_download_box_feeds'));
+	$list = get_option('moz_download_box_feeds');
+
+	
+	foreach ($list as $feed=>$data) 
+		moz_download_box_update_table($feed);
+}
+
+function moz_download_box_update_table($feed) {
 	global $wpdb;
+
+	$feed_info = moz_download_box_get_feed_details($feed);
 	
 	if($wpdb->get_var("SHOW TABLES LIKE '{$wpdb->prefix}moz_download_box'") != $wpdb->prefix . 'moz_download_box') 
 		moz_download_box_construct_table();
 
-	$version_tags = array_flip(moz_download_box_fetch_tag_information($versions_url));
-	$data         =            moz_download_box_fetch_version_information($primary_builds_url);
+//print("==="); var_dump($feed_info); //debug
+
+	$tags = moz_download_box_fetch_tag_information($feed_info['tags_url']);
+	$data         =            moz_download_box_fetch_version_information($feed_info['builds_url']);
 	
-	$items = moz_download_box_fetch_json ($version_tags, $data);
-	unset ($data);
+	if (count($tags) < 1 || count($data) < 1) {
+		echo ("unable to fetch feed data!");
+	}
+	else {
+		$version_tags = array_flip($tags);
+
+		moz_download_box_update_feed_tags($feed, $tags);
 	
-	if (count($items) > 0) 	foreach ($items as $tag => $items) {
+		$items = moz_download_box_fetch_json ($version_tags, $data);
+		unset ($data);
+	
+		if (count($items) > 0) 	foreach ($items as $tag => $items) {
 //		$sql = "DELETE FROM {$wpdb->prefix}moz_download_box WHERE tag='$tag'; "; 
 //		$wpdb->query($sql);
 		
-		$sql = "INSERT INTO {$wpdb->prefix}moz_download_box ( tag, locale, version, platform, filesize, unavailable )	VALUES ";
+			$sql = "INSERT INTO {$wpdb->prefix}moz_download_box ( tag, locale, version, platform, filesize, unavailable )	VALUES ";
 		
-		foreach ($items as $item) {
+			foreach ($items as $item) {
 
 // Single queries are evil
 //			$sql = $wpdb->prepare(
@@ -223,23 +524,26 @@ function moz_download_box_update_table($primary_builds_url, $versions_url) {
 //				$item['tag'], $item['locale'], $item['version'], $item['os'], $item['filesize'], $item['unavailable']);
 //				$wpdb->query($sql);
 
-			$sql .= $wpdb->prepare ("( %s,   %s,     %s,       %s,        %s,         %d),",$item['tag'], $item['locale'], $item['version'], $item['os'], $item['filesize'], $item['unavailable']);
-		}
+				$sql .= $wpdb->prepare ("( %s,   %s,     %s,       %s,        %s,         %d),",$item['tag'], $item['locale'], $item['version'], $item['os'], $item['filesize'], $item['unavailable']);
+			}
 
-		$wpdb->query("DELETE FROM {$wpdb->prefix}moz_download_box WHERE tag='$tag';");
-		$sql[strlen($sql)-1] = ';'; // Replace last comma with semicolon. 
-		$wpdb->query($sql);
+			$wpdb->query("DELETE FROM {$wpdb->prefix}moz_download_box WHERE tag='$tag';");
+			$sql[strlen($sql)-1] = ';'; // Replace last comma with semicolon. 
+			$wpdb->query($sql);
+		
+			moz_download_box_update_feed_timestamp($feed, time());
+		}
+		else echo ("nothing has been fetched. Sorry... :(\n");
 	}
-	else echo ("nothing has been fetched. Sorry... :(\n");	
 }
 	
 function moz_download_box_fetch_json($version_tag = array(), $data = array()) {
 
 	$fetch = array();
 	
-	foreach ($data as $locale=>$item) {
-		foreach ($item as $version=>$item) {
-			foreach ($item as $platform=>$item) {
+	foreach ($data as $locale => $item) {
+		foreach ($item as $version => $item) {
+			foreach ($item as $platform => $item) {
 				if (isset($item['unavailable']) && $item['unavailable'] == 'true') $unavailable = 1; 
 				else $unavailable = 0; 
 				
@@ -264,13 +568,10 @@ function moz_download_box_query ($tag = NULL, $locale = NULL, $os = NULL) {
     if ($os) $query .= "os='$os' AND ";
     $query .= '1=1;';
     
-    echo $query;
+//    echo $query;
 
 	return $wpdb->get_results($query, ARRAY_A); 
 }
-
-
-
 
 function moz_download_box_button_shortcode($atts, $content=null, $code="") {
    // $atts    ::= array of attributes
@@ -296,4 +597,5 @@ function moz_download_box_button_shortcode($atts, $content=null, $code="") {
   
   return $out;
 }
+
 
